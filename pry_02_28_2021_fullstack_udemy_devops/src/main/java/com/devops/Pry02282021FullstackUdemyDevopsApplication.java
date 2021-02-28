8579d31d349f9388f11df4d4703d111ab194ef2d
Freddy <freddy.alexis.ht@gmail.com>
2021-02-28 14:50:06 -0500

    Added Spring Boot project

---
+++ pry_02_28_2021_fullstack_udemy_devops/src/main/java/com/devops/Pry02282021FullstackUdemyDevopsApplication.java
@@ -0,0 +1,13 @@
+package com.devops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pry02282021FullstackUdemyDevopsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pry02282021FullstackUdemyDevopsApplication.class, args);
+	}
+
+}
