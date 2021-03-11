ebb8281027118bfa30d8cf24086a6d945860e4b9
Freddy <freddy.alexis.ht@gmail.com>
2021-03-10 20:10:31 -0500

    Enabled Spring Security for the website

---
+++ pry_02_28_2021_fullstack_udemy_devops/src/main/java/com/devops/config/SecurityConfig.java
@@ -0,0 +1,57 @@
+package com.devops.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    /** Public URLs. */
+    private static final String[] PUBLIC_MATCHERS = {
+            "/webjars/**", // bootstrap & jquery libraries
+            "/css/**",
+            "/js/**",
+            "/images/**", // todo el contenido estático
+            "/",		  // home page
+            "/about/**",
+            "/contact/**",
+            "/error/**/*",
+    };
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers(PUBLIC_MATCHERS).permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin().loginPage("/login").defaultSuccessUrl("/payload")
+                .failureUrl("/login?error").permitAll()
+                .and()
+                .logout().permitAll();
+    }
+
+    // Con usar la dependencia de Spring-Security este bean está disponible
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .inMemoryAuthentication()
+                .withUser("user").password("password")
+                .roles("USER");
+    }
+    
+    @Bean
+    public PasswordEncoder getPasswordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }   
+
+	
+}
