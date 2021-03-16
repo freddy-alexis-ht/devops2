e443c47fdc5bb0f567751a9e460b6f5e62270b9e
Freddy <freddy.alexis.ht@gmail.com>
2021-03-16 15:17:21 -0500

    Completed the Forgot My Password journey implementation

--- pry_02_28_2021_fullstack_udemy_devops/src/main/java/com/devops/web/controllers/ForgotMyPasswordController.java
+++ pry_02_28_2021_fullstack_udemy_devops/src/main/java/com/devops/web/controllers/ForgotMyPasswordController.java
@@ -1,12 +1,20 @@
 package com.devops.web.controllers;
 
+import java.time.Clock;
+import java.time.LocalDateTime;
+import java.util.Locale;
+
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -16,6 +24,7 @@ import com.devops.backend.persistence.domain.backend.User;
 import com.devops.backend.service.EmailService;
 import com.devops.backend.service.I18NService;
 import com.devops.backend.service.PasswordResetTokenService;
+import com.devops.backend.service.UserService;
 import com.devops.utils.UserUtils;
 
 @Controller
@@ -34,6 +43,12 @@ public class ForgotMyPasswordController {
 
 	public static final String EMAIL_MESSAGE_TEXT_PROPERTY_NAME = "forgotmypassword.email.text";
 
+	public static final String CHANGE_PASSWORD_VIEW_NAME = "forgotmypassword/changePassword";
+
+    private static final String PASSWORD_RESET_ATTRIBUTE_NAME = "passwordReset";
+
+    private static final String MESSAGE_ATTRIBUTE_NAME = "message";
+	
 	@Autowired
 	private I18NService i18NService;
 	
@@ -42,7 +57,10 @@ public class ForgotMyPasswordController {
 
 	@Autowired
 	private PasswordResetTokenService passwordResetTokenService;
-	
+
+	@Autowired
+	private UserService userService;
+	   
 	@Value("${webmaster.email}")
 	private String webMasterEmail;
 	
@@ -92,5 +110,101 @@ public class ForgotMyPasswordController {
 		
 		return EMAIL_ADDRESS_VIEW_NAME;
 	}
+	
+	// Los param id y token se obtienen del link que se envió al correo
+	// Spring automáticamente completará los otros dos parámetros
+    @RequestMapping(value = CHANGE_PASSWORD_PATH, method = RequestMethod.GET)
+    public String changeUserPasswordGet(@RequestParam("id") long id,
+                                        @RequestParam("token") String token,
+                                        Locale locale,
+                                        ModelMap model) {
+    	// Primero se verifica que el token no sea null ni esté vacío, el id no debe ser 0
+        // org.springframework.util.StringUtils
+    	if (StringUtils.isEmpty(token) || id == 0) {
+            LOG.error("Invalid user id {}  or token value {}", id, token);
+            model.addAttribute(PASSWORD_RESET_ATTRIBUTE_NAME, "false");
+            model.addAttribute(MESSAGE_ATTRIBUTE_NAME, "Invalid user id or token value");
+            return CHANGE_PASSWORD_VIEW_NAME;
+        }
+	
+    	// El token no está vacío ni es nulo ni el id es 0
+        PasswordResetToken passwordResetToken = passwordResetTokenService.findByToken(token);
+
+        // Si el token ingresado no es encontrado en la DB
+        if (null == passwordResetToken) {
+            LOG.warn("A token couldn't be found with value {}", token);
+            model.addAttribute(PASSWORD_RESET_ATTRIBUTE_NAME, "false");
+            model.addAttribute(MESSAGE_ATTRIBUTE_NAME, "Token not found");
+            return CHANGE_PASSWORD_VIEW_NAME;
+        }
+
+        // Si el token sí existe en la DB, se extrae el objeto-User
+        User user = passwordResetToken.getUser();
+
+        // Se evalúa si el userId del link coincide con el de la DB
+        // Esto protege a la app de ser invocado por alguien que no haya recibido el email
+        if (user.getId() != id) {
+            LOG.error("The user id {} passed as parameter does not match the user id {} associated with the token {}",
+                    id, user.getId(), token);
+            model.addAttribute(PASSWORD_RESET_ATTRIBUTE_NAME, "false");
+            model.addAttribute(MESSAGE_ATTRIBUTE_NAME, i18NService.getMessage("resetPassword.token.invalid", locale));
+            return CHANGE_PASSWORD_VIEW_NAME;
+        }
+
+        // Se compara la fecha de expiración con la fecha actual
+        if (LocalDateTime.now(Clock.systemUTC()).isAfter(passwordResetToken.getExpiryDate())) {
+            LOG.error("The token {} has expired", token);
+            model.addAttribute(PASSWORD_RESET_ATTRIBUTE_NAME, "false");
+            model.addAttribute(MESSAGE_ATTRIBUTE_NAME, i18NService.getMessage("resetPassword.token.expired", locale));
+            return CHANGE_PASSWORD_VIEW_NAME;
+        }
+
+        // Llegado a este punto quiere decir que todo está correcto.
+        // Este valor será usado en el método POST posterior, cuando el ususario quiera resetear el password
+        model.addAttribute("principalId", user.getId());
+
+        // OK to proceed. We auto-authenticate the user so that in the POST request we can check if the user
+        // is authenticated
+        // import org.springframework.security.core.Authentication;
+        Authentication auth = new UsernamePasswordAuthenticationToken(
+                user, null, user.getAuthorities());
+        SecurityContextHolder.getContext().setAuthentication(auth);
+
+        return CHANGE_PASSWORD_VIEW_NAME;
+    }
+	
+    // arg-1 .. que viene del input.hidden del form
+    // arg-2 .. el nuevo password
+    @RequestMapping(value = CHANGE_PASSWORD_PATH, method = RequestMethod.POST)
+    public String changeUserPasswordPost(@RequestParam("principal_id") long userId,
+                                         @RequestParam("password") String password,
+                                         ModelMap model) {
+
+    	// Si el usuario no está autenticado, en este punto podría considerarse un security-breach
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (null == authentication) {
+            LOG.error("An unauthenticated user tried to invoke the reset password POST method");
+            model.addAttribute(PASSWORD_RESET_ATTRIBUTE_NAME, "false");
+            model.addAttribute(MESSAGE_ATTRIBUTE_NAME, "You are not authorized to perform this request.");
+            return CHANGE_PASSWORD_VIEW_NAME;
+        }
+
+        User user = (User) authentication.getPrincipal();
+        if (user.getId() != userId) {
+            LOG.error("Security breach! User {} is trying to make a password reset request on behalf of {}",
+                    user.getId(), userId);
+            model.addAttribute(PASSWORD_RESET_ATTRIBUTE_NAME, "false");
+            model.addAttribute(MESSAGE_ATTRIBUTE_NAME, "You are not authorized to perform this request.");
+            return CHANGE_PASSWORD_VIEW_NAME;
+        }
+
+        userService.updateUserPassword(userId, password);
+        LOG.info("Password successfully updated for user {}", user.getUsername());
+
+        model.addAttribute(PASSWORD_RESET_ATTRIBUTE_NAME, "true");
+
+        return CHANGE_PASSWORD_VIEW_NAME;
+
+    }
 	
 }
